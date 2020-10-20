2c77d8a30ce5243344b15115c4fed29067e01354
Laercio-pc <lndsilva@hotmail.com>
2020-10-19 22:18:10 -0300

    grupo de menu e estilos

--- app/src/main/java/br/com/local/appcomponentes2h/Hulk_Activity.java
+++ app/src/main/java/br/com/local/appcomponentes2h/Hulk_Activity.java
@@ -3,6 +3,8 @@ package br.com.local.appcomponentes2h;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Hulk_Activity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Hulk_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hulk_layout);
+    }
+
+    //Adicionando o menu na janela
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Instânciar o menu na barra do app
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
